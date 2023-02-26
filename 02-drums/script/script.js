cb057c1d397a1bd5e217a772021bad22bcd2d179
Natalia Ivantsova <ivantsovana@gmail.com>
2023-02-26 23:04:35 +0200

    added js to drums project

--- 02-drums/script/script.js
+++ 02-drums/script/script.js
@@ -1,9 +1,24 @@
 const drumItem = document.querySelectorAll(".drum__item");
+const keys = document.querySelectorAll(".drum__key");
 
 drumItem.forEach(function (el) {
   el.addEventListener("click", playSound);
 })
 
 function playSound() {
-  this.style.color = 'black';
+  let audioName = this.lastElementChild.innerText.toLowerCase();
+  audioFile = new Audio(`./media/${audioName}.wav`);
+  audioFile.play();
+}
+
+document.body.addEventListener("keypress", submitKeypress);
+
+function submitKeypress(e) {
+  for (let i = 0; i < keys.length; i++) {
+    if (e.key === keys[i].innerText.toLowerCase()) {
+      let audioName = keys[i].nextElementSibling.innerText.toLowerCase();
+      audioFile = new Audio(`./media/${audioName}.wav`);
+      audioFile.play();
+    }
+  }
 }
